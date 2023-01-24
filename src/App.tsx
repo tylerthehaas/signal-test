ac7f8b2ab80789ab409883d7653ea0ebf3ca6fd9
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { signal, useSignal } from "@preact/signals-react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const count = signal(0);
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>React + Vite</h1>
+      <h2>On CodeSandbox!</h2>
+      <div className="card">
+        <button
+          onClick={() => {
+            count.value++;
+          }}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR.
+        </p>
+
+        <p>
+          Tip: you can use the inspector button next to address bar to click on
+          components in the preview and open the code in the editor!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
